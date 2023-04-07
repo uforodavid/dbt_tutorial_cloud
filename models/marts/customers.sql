69fa14dac8dd19bcade05e11e91fa9cf87443f3a
uforodavid4@gmail.com <uforodavid4@gmail.com>
2023-04-07 20:32:20 +0000

    update

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -58,7 +58,8 @@ final as (
         customer_orders.first_order,
         customer_orders.most_recent_order,
         customer_orders.number_of_orders,
-        customer_payments.total_amount as customer_lifetime_value
+        customer_payments.total_amount as customer_lifetime_value,
+        customers.first_name = "{{ var('ceo') }}" as is_ceo
 
     from customers
 
